a8f843b40e7aea5bf4214cd9d53a9f902aa52508
jhalto <zobayerarmannadim@gmail.com>
2024-09-18 01:16:21 +0600

    contact list view done

---
+++ lib/home.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:sqflite_practice/add_contact.dart';
+import 'package:sqflite_practice/db_helper.dart';
+
+import 'contact.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Contact List"),centerTitle: true,),
+      body: FutureBuilder(
+          future: DbHelper().readContact(), builder: (BuildContext context,AsyncSnapshot<List<Contact>> snapshot){
+           if(!snapshot.hasData){
+             return Center(
+               child: Column(
+                 children: [
+                   CircularProgressIndicator(
+                   ),
+                   Text('Loading')
+                 ],
+               ),
+             );
+           }
+           return snapshot.data!.isEmpty? Center(
+             child: Text("data nai"),
+           ):ListView(
+             children: snapshot.data!.map((e) => Center(
+               child: ListTile(
+                 title: Text(e.name),
+                 subtitle: Text(e.phone),
+                 trailing: IconButton(onPressed: (){}, icon: Icon(Icons.delete)),
+               ),
+             ),).toList(),
+           );
+      }),
+      floatingActionButton: FloatingActionButton(onPressed: (){
+        Navigator.push(context, MaterialPageRoute(builder: (context) => AddContact(),));
+      },child: Icon(Icons.add),),
+    );
+  }
+}
